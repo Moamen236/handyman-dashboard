2f0ce301252a788b2d3bb5a3da2b298b0a9f97e3
Moamen236 <moamen.ali107@gmail.com>
2025-03-16 04:58:57 +0200

    Customers View

--- assets/js/pages/datatables.init.js
+++ assets/js/pages/datatables.init.js
@@ -1,9 +1,33 @@
+// $(document).ready(function () {
+//     $("#datatable").DataTable(),
+//         $("#datatable-buttons")
+//             .DataTable({ buttons: ["copy", "excel", "pdf"], dom: 'Plfrtip',lengthChange: !1 })
+//             .buttons()
+//             .container()
+//             .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),
+//         $(".dataTables_length select").addClass("form-select form-select-sm");
+// });
+
+
 $(document).ready(function () {
-    $("#datatable").DataTable(),
-        $("#datatable-buttons")
-            .DataTable({ buttons: ["copy", "excel", "pdf"], dom: 'Plfrtip',lengthChange: !1 })
-            .buttons()
-            .container()
-            .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),
-        $(".dataTables_length select").addClass("form-select form-select-sm");
+    $("#datatable").DataTable({
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json"
+        }
+    });
+
+    $("#datatable-buttons")
+        .DataTable({
+            buttons: ["copy", "excel", "pdf"],
+            dom: 'Plfrtip',
+            lengthChange: false,
+            language: {
+                url: "https://cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json"
+            }
+        })
+        .buttons()
+        .container()
+        .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+
+    $(".dataTables_length select").addClass("form-select form-select-sm");
 });
